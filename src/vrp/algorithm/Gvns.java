6b6edc3fe80d8a2a5541bca8776195997239ad8d
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-04-25 13:54:33 +0100

    added gvns

---
+++ src/vrp/algorithm/Gvns.java
@@ -0,0 +1,15 @@
+/**
+ * Universidad de La Laguna
+ * Diseño y análisis de algoritmos
+ * Grado en Ingeniería Informática
+ *
+ * @author: Daniel Hernández de León
+ * @since 09/04/2022
+ * @version 1.0.0
+ */
+
+package vrp.algorithm;
+
+public class Gvns {
+  
+}
